a2a44c198e17440c6ae5a4c1628ace3fa129b96e
GraceNam <zeidgame@gmail.com>
2021-05-16 21:45:27 +0900

    feature : 뼈대
    <description>
    + README 작성
      + 설계를 하기 위한 내용 추가
    + 기능 별 뼈대 추가
      + 각 기능별 class 생성
    <issue-number>

---
+++ src/main/java/baseball/domain/Judge.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Judge {
+
+    public int correctCount(List<Integer> computer, List<Integer> player) {
+        return 0;
+    }
+
+    public boolean hsaPlace(int placeNumber, int number) {
+        return false;
+    }
+
+}
